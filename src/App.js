ab5433518714a37141b9815cd4207217e3b4863d
Dharmesh Patel <dharmesh@wonderschool.com>
2019-11-13 00:29:30 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container style={{backgroundColor: '#eee', height: '100vh' }} >
+        <div>hi</div>
+      </Container>
+    )
+  }
+}
